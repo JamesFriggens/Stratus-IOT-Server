bf4c70cff078392717c66fc8886f9bfb5e14dfbe
James <jfriggs1877@gmail.com>
2024-08-16 19:50:43 -0400

    update TODO list with goals import pandas

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # TODO
-# handle data from device
+# handle ip and name from device and send most recent ips and names it needs to function
 # store data into csv
-# if connected device has the same name but different ip, then update the entry
+# handle csv data with pandas data frame
+# if connected device has the same name but different ip, then update the ip
+# log all unique connections from devices
 # create new thread for each device that connects
 
 import socket
 import datetime
 import threading
+import pandas
 
 ip_address = "0.0.0.0"
 port = 10002
